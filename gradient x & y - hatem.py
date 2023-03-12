7fe90c8ab9e77a658e9d394ca112b5e4a476a4fd
احمد حاتم فاروق عبد الباقى <ahm45796@gmail.com>
2023-03-12 20:00:54 +0200

    still want to find a solution to get gradient x and y - hatem

--- gradient x & y - hatem.py
+++ gradient x & y - hatem.py
@@ -3,11 +3,16 @@ import numpy as np
 import cv2
 
 
-img = plt.imread('letterR.jpg')
+img = plt.imread('phantom.jpg')
+img = img/255
 dimensions = img.shape
 print(dimensions)
+print(np.min(img))
+print(np.max(img))
 
 
+
+"""
 width = int(img.shape[1] * 64 / 512)
 height = int(img.shape[0] * 64 / 512)
 dim = (width, height)
@@ -15,50 +20,60 @@ dim = (width, height)
 # resize image
 resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
 print(resized.shape)
-
-image = resized[:,:,2]
+"""
+image = np.zeros((64, 64, 3))
+image[:,:,2] = img[:,:,2]
+plt.figure(figsize=(5, 5))
+plt.imshow(image, cmap='gray')
+plt.show()
+"""
 img_transformed = np.zeros((64, 64,3), dtype=np.uint8)
 img_transformed[:, :, 2] = image
+"""
+kSpace = np.zeros((64, 64), dtype=np.complex_)
 
-
-
-def Rotation_z(theta):
+def equ_of_Rotation_z(theta):
     rotation_z = np.array(
-        [[int(np.cos(theta)), int(-np.sin(theta)), 0], [int(np.sin(theta)), int(np.cos(theta)), 0], [0, 0, 1]])
+        [[int(np.cos(theta)), int(-np.sin(np.radians(theta))), 0], [int(np.sin(np.radians(theta))), int(np.cos(np.radians(theta))), 0], [0, 0, 1]])
     return rotation_z
 
-Rf_theta =  np.radians(90)
-rotation_x = np.array([[1, 0, 0], [0, int(np.cos(Rf_theta)), int(-np.sin(Rf_theta))], [0, int(np.sin(Rf_theta)), int(np.cos(Rf_theta))]])
+def equ_of_Rotation_x(theta):
+    rotation_x = np.array([[1, 0, 0], [0, int(np.cos(np.radians(theta))), int(-np.sin(np.radians(theta)))], [0, int(np.sin(np.radians(theta))), int(np.cos(np.radians(theta)))]])
+    return rotation_x
 
 
-
-
+"""
 def Rotation_x(image):
-    for i, row in enumerate(image):
-        for j, col in enumerate(row):
-            pixel_data = image[i, j, :]
-            input_coords = np.array([i, j, 1])
-            i_out, j_out, _ = rotation_x @ input_coords
-            img_transformed[i_out, j_out, :] = pixel_data
+    for i in range(0, image.shape[0]):
+        for j in range(0, image.shape[1]):
+            img_transformed[i,j] = np.dot(equ_of_Rotation_x(90),img_transformed[i,j])
+
+    return img_transformed
 
 
-Rotation_x(resized)
 
 
+
+for A in range(0, img.shape[0]):
+    Rotation_x(img)
+    for B in range(0, img.shape[1]):
+        step_of_Y = (360 / img.shape[0]) * B
+        step_of_X = (360 / img.shape[1]) * A
+        for i in range(0, img.shape[0]):
+            for j in range(0, img.shape[1]):
+                phase = step_of_Y * j + step_of_X * i
+                img_transformed[i,j] = np.dot(equ_of_Rotation_z(phase),img_transformed[i,j])
+                sumOfRows = np.sum(img_transformed[:, :, 0])
+                sumOfColumns = np.sum(img_transformed[:, :, 1])
+                complex_value = np.complex(sumOfRows,sumOfColumns)
+                kSpace[i,j] = complex_value
+
+
+kSpace = np.fft.fft2(kSpace)
+plt.figure(figsize=(5, 5))
+plt.imshow(np.abs(kSpace), cmap='gray')
+plt.show()
 """
-for I in range(0, resized.shape[0]):
-    for J in range(0, resized.shape[1]):
-        step_of_Y = (360 / resized.shape[0]) * I
-        step_of_X = (360 / resized.shape[1]) * J
-        for i, row in enumerate(img_transformed):
-            for j, col in enumerate(row):
-                pixel_data = img_transformed[i, j, :]
-                input_coords = np.array([i, j, 1])
-                phase = step_of_Y * i + step_of_X * j
-                i_out, j_out, _ = Rotation_z(np.radians(phase)) @ input_coords
-                img_transformed[i_out, j_out, :] = pixel_data
-"""
-
 
 """
 for gradient_y_phase in range(0, 360, 30):
@@ -79,6 +94,4 @@ for gradient_y_phase in range(0, 360, 30):
 
 
 
-plt.figure(figsize=(5, 5))
-plt.imshow(img_transformed, cmap='gray')
-plt.show()
+
