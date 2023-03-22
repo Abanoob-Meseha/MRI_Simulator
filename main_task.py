ff6b3c08a38bb40639d9a3a062de85d1f216a9a5
احمد حاتم فاروق عبد الباقى <ahm45796@gmail.com>
2023-03-22 21:20:55 +0200

    make comments on the functions and change some names of variables

--- main_task.py
+++ main_task.py
@@ -277,24 +277,24 @@ class MainWindow(QtWidgets.QMainWindow):
     def Draw_Sequence(self, df):
         self.plot_Const_Lines()
         # plotting functions of Rf,Gz,Gy,Gx,Ro
-        x1 = np.linspace(df["RF1_Ts"].values[0], df["RF1_Te"].values[0], 1000)
-        y1 = self.Rf_line + ((df["RF1_value"].values[0]) * np.sinc(x1 - 10))
+        x_rf = np.linspace(df["RF1_Ts"].values[0], df["RF1_Te"].values[0], 1000)
+        y_rf = self.Rf_line + ((df["RF1_value"].values[0]) * np.sinc(x_rf - 10))
 
-        x5 = np.linspace(df["Ro_Ts"].values[4], df["Ro_Te"].values[4], 1000)
-        y5 = self.Ro_line + ((df["Ro_value"].values[4]) * np.sinc(x5 - 55))
+        x_ro = np.linspace(df["Ro_Ts"].values[4], df["Ro_Te"].values[4], 1000)
+        y_ro = self.Ro_line + ((df["Ro_value"].values[4]) * np.sinc(x_ro - 55))
 
-        x6 = np.linspace(df["RF2_Ts"].values[5], df["RF2_Te"].values[5], 1000)
-        y6 = self.Rf_line + ((df["RF2_value"].values[5]) * np.sinc(x1 - 10))
+        x_rf2 = np.linspace(df["RF2_Ts"].values[5], df["RF2_Te"].values[5], 1000)
+        y_rf2 = self.Rf_line + ((df["RF2_value"].values[5]) * np.sinc(x_rf2 - 80))
 
-        self.sequenceCanvas.axes.plot(x1, y1, color='maroon', marker='o')
+        self.sequenceCanvas.axes.plot(x_rf, y_rf, color='maroon', marker='o')
         self.sequenceCanvas.axes.step(x=[df["Gz_Ts"].values[1], df["Gz_Te"].values[1], df["Gz_Te"].values[1]],
                  y=[self.Gz_line, (self.Gz_line + 1) * df["Gz_value"].values[1], self.Gz_line])
         self.sequenceCanvas.axes.step(x=[df["Gy_Ts"].values[2], df["Gy_Te"].values[2], df["Gy_Te"].values[2]],
                  y=[self.Gy_line, (self.Gy_line + 1) * df["Gy_value"].values[2], self.Gy_line])
         self.sequenceCanvas.axes.step(x=[df["Gx_Ts"].values[3], df["Gx_Te"].values[3], df["Gx_Te"].values[3]],
                  y=[self.Gx_line, (self.Gx_line + 1) * df["Gx_value"].values[3], self.Gx_line])
-        self.sequenceCanvas.axes.plot(x5, y5, color='maroon', marker='o')
-        self.sequenceCanvas.axes.plot(x6, y6, color='maroon', marker='o')
+        self.sequenceCanvas.axes.plot(x_ro, y_ro, color='maroon', marker='o')
+        self.sequenceCanvas.axes.plot(x_rf2, y_rf2, color='maroon', marker='o')
 
 
         # Plotting repeat of Gy if it exists
@@ -352,12 +352,14 @@ class MainWindow(QtWidgets.QMainWindow):
         final_image[:, :, 2] = normalized_img
         return final_image
 
+    # matrix of rotation z
     def equ_of_Rotation_z(self, theta):
         rotation_z = np.array(
             [[np.cos(np.radians(theta)), -np.sin(np.radians(theta)), 0],
              [np.sin(np.radians(theta)), np.cos(np.radians(theta)), 0], [0, 0, 1]])
         return rotation_z
 
+    # matrix of rotation x
     def equ_of_Rotation_x(self, theta):
         rotation_x = np.array([[1, 0, 0], [0, np.cos(np.radians(theta)), -np.sin(np.radians(theta))],
                                [0, np.sin(np.radians(theta)), np.cos(np.radians(theta))]])
@@ -373,7 +375,16 @@ class MainWindow(QtWidgets.QMainWindow):
         return rotated_image
 
     # Reconstrucing the image and generating kspace
+    # you will find the number of the step in the function
+    # step 1 : select the phantom size and modify image and get the flip angle
+    # step 2 : start looping over rows and columns of the phantom image and calling the function of rotation x that means we hit rf signal with our flip angle
+    # step 3 : we made the phase to make the rotation z (gradient x and y) with it and apply gradient on rows and columns of the image
+    # step 4 : we get gradient image, make sumation of values x and y and make the complex value (kspace)
+    # step 5 : we plot kspace and image after reconstruction
     def reconstruct_image(self):
+        # step 1
+        self.Kspace_graph.axes.clear()
+        self.Reconstructedimage_graph.axes.clear()
         # choosing size of phantom
         if self.phantomSize_comboBox.currentIndex()==0:
             phantomImg = shepp_logan(16)
@@ -387,34 +398,38 @@ class MainWindow(QtWidgets.QMainWindow):
         kSpace = np.zeros((phantomImg.shape[0], phantomImg.shape[1]), dtype=np.complex_)
         modified_img = self.modify_image(phantomImg)
         Phase_of_X = self.get_Flip_angle()
-        for A in range(0, modified_img.shape[0]):
+        # step 2
+        for R in range(0, modified_img.shape[0]):
             rotated_matrix = self.Rotation_x(modified_img, Phase_of_X)
-            for B in range(0, modified_img.shape[1]):
-                step_of_Y = (360 / modified_img.shape[0]) * B
-                step_of_X = (360 / modified_img.shape[1]) * A
+            for C in range(0, modified_img.shape[1]):
+                # step 3
+                step_of_Y = (360 / modified_img.shape[0]) * C
+                step_of_X = (360 / modified_img.shape[1]) * R
                 Final_matrix = np.zeros(modified_img.shape)
                 #Applying rotation z in x&y plane
                 for i in range(0, modified_img.shape[0]):
                     for j in range(0, modified_img.shape[1]):
                         phase = step_of_Y * j + step_of_X * i
                         Final_matrix[i, j] = np.dot(self.equ_of_Rotation_z(phase), rotated_matrix[i, j])
+                # step 4
                 #Getting the value of kspace
                 gradient_image = Final_matrix
                 sum_of_x = np.sum(gradient_image[:, :, 0])
                 sum_of_y = np.sum(gradient_image[:, :, 1])
                 complex_value = np.complex(sum_of_x, sum_of_y)
-                kSpace[A, B] = complex_value
+                kSpace[R, C] = complex_value
 
             Final_img = np.zeros((phantomImg.shape[0], phantomImg.shape[1], 3))
             Final_img[:, :, 2] = phantomImg
+            # step 5
             self.Kspace_graph.axes.imshow(np.abs(kSpace), cmap='gray')
             self.Kspace_graph.draw()
-            self.Kspace_graph.start_event_loop(0.1)
+            self.Kspace_graph.start_event_loop(0.0005)
             Reconstructed_image = np.fft.fft2(kSpace)
             self.Reconstructedimage_graph.axes.imshow(np.abs(Reconstructed_image), cmap='gray')
             self.Reconstructedimage_graph.draw()
-            self.Reconstructedimage_graph.start_event_loop(0.1)
-            print(A)
+            self.Reconstructedimage_graph.start_event_loop(0.0005)
+            print(R)
 
 
 if __name__ == '__main__':
